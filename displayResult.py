d60faa776ad803b640e30303bcf8604ab7919560
Tyson Norman <tnorman75@icloud.com>
2025-08-03 17:10:52 -0500

    added variable message

--- displayResult.py
+++ displayResult.py
@@ -18,7 +18,7 @@ def display_results(response, lat, lon):
             st.markdown(
                 """
                 <div style="padding: 1em; background-color: #e0f3ff; border-radius: 8px; color: #0b3d91;">
-                <strong>Info:</strong> This is a light blue message box similar to st.success().
+                <strong>Info:</strong> {error_message}
                 </div>
                 """,
             unsafe_allow_html=True
