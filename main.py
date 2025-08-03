627de49fbb8717c126560e99126ec01e53e86058
Tyson Norman <tnorman75@icloud.com>
2025-08-03 07:03:52 -0500

    button update

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ endpoint = "/getborderproximity"
 params = {"latitude": lat_value, "longitude": lon_value, "country": selected_country}
 
 # Create columns: [left spacer, col1, tiny spacer, col2, right spacer]
-spacer_left, butcol1, spacer_mid, butcol2, spacer_right = st.columns([1, 3, 0.1, 3, 1])
+butcol1, spacer_mid, butcol2, spacer_right = st.columns([3, 0.1, 3, 1])
 
 
 if isinstance(lat_value, (int, float)) and isinstance(lon_value, (int, float)):
