d71c359d9b895f4bbbc5e5288bd0d309b47415a5
Tyson Norman <tnorman75@icloud.com>
2025-08-03 07:53:54 -0500

    format changes

--- displayResult.py
+++ displayResult.py
@@ -13,12 +13,12 @@ def display_results(response, selected_country):
             st.error(f"{errorMessage}")
         elif notInCountry is not None:
             if selected_country == "United States of America":
-                st.error(f"The specified location is not within the **United States&#39;s** borders. It is located in {locatedCountry}.")
+                st.error(f"The specified location is not within the **United States&#39;s** borders. It is located in **{locatedCountry}**.")
             else:
                 if locatedCountry == "United States":
-                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in the United States.")
+                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in the **United States**.")
                 else:
-                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in {locatedCountry}.")
+                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in **{locatedCountry}**.")
         else:
             if selected_country == "United States of America":
                 st.success(f"Object is **{distance_miles}** miles ({distance_km} km) from the closest border of the United States.")
