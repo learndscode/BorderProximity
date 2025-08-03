10bc373a919c44b81ae224b4bb7277fbd4357f9c
Tyson Norman <tnorman75@icloud.com>
2025-08-03 17:09:24 -0500

    removed border

--- displayResult.py
+++ displayResult.py
@@ -14,9 +14,10 @@ def display_results(response, lat, lon):
         elif notInCountry is not None:
             st.error(f"The specified location is not within the borders of a country. It is likely on a boat or at the bottom of the ocean.")
             error_message = "The specified location is not within the borders of a country. It is likely on a boat or at the bottom of the ocean."
+            #border-left: 5px solid #2196F3;
             st.markdown(
                 """
-                <div style="padding: 1em; background-color: #e0f3ff; border-left: 5px solid #2196F3; border-radius: 4px; color: #0b3d91;">
+                <div style="padding: 1em; background-color: #e0f3ff; border-radius: 4px; color: #0b3d91;">
                 <strong>Info:</strong> This is a light blue message box similar to st.success().
                 </div>
                 """,
