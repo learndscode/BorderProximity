45f115cd65c07d0f17c1f175ce68d7305deeea7e
Tyson Norman <tnorman75@icloud.com>
2025-08-03 07:47:20 -0500

    update response formats to include located country

--- displayResult.py
+++ displayResult.py
@@ -4,6 +4,7 @@ def display_results(response, selected_country):
     if response.status_code == 200:
         # Check if result is not in country
         notInCountry = response.json().get("notincountry")
+        locatedCountry = response.json().get("locatedcountry")
         errorMessage = response.json().get("error")
         distance_miles = response.json().get("distance_miles")
         distance_km = response.json().get("distance_km")
@@ -12,9 +13,12 @@ def display_results(response, selected_country):
             st.error(f"{errorMessage}")
         elif notInCountry is not None:
             if selected_country == "United States of America":
-                st.error(f"The specified location is not within the **United States&#39;s** borders.")
+                st.error(f"The specified location is not within the **United States&#39;s** borders. It is located in {locatedCountry}.")
             else:
-                st.error(f"The specified location is not within **" + selected_country + "&#39;s** borders.")
+                if locatedCountry == "United States of America":
+                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in the United States.")
+                else:
+                    st.error(f"The specified location is not within **{selected_country}&#39;s** borders. It is located in {locatedCountry}.")
         else:
             if selected_country == "United States of America":
                 st.success(f"Object is **{distance_miles}** miles ({distance_km} km) from the closest border of the United States.")
