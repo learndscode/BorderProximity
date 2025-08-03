49a0e44889669e21d69201455c027217c7457b8b
Tyson Norman <tnorman75@icloud.com>
2025-08-03 17:12:44 -0500

    removed info

--- displayResult.py
+++ displayResult.py
@@ -18,7 +18,7 @@ def display_results(response, lat, lon):
             st.markdown(
                 f"""
                 <div style="padding: 1em; background-color: #e0f3ff; border-radius: 8px; color: #0b3d91;">
-                <strong>Info:</strong> {error_message}
+                {error_message}
                 </div>
                 """,
             unsafe_allow_html=True
